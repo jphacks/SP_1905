56825e664247a8e0408d2b12a7f217030113d2fe
Fuyan777 <fuyamiwa713@gmail.com>
2019-10-26 14:35:00 +0900

    add emptyCell

---
+++ office-hunting/office-hunting/Message/EmptyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmptyTableViewCell.swift
+//  office-hunting
+//
+//  Created by 山田楓也 on 2019/10/26.
+//
+
+import UIKit
+
+class EmptyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- office-hunting/office-hunting/Message/MessageViewController.swift
+++ office-hunting/office-hunting/Message/MessageViewController.swift
@@ -7,23 +7,73 @@
 
 import UIKit
 
-class MessageViewController: UIViewController {
-
+class MessageViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    var messageImage: UIImage!
+    var centerImage: UIImageView!
+    var msgData:[String] = []
+    @IBOutlet weak var msgTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //setNavImage()
+        
+        self.messageImage = UIImage(named: "message.png")
+        self.centerImage = UIImageView(image: messageImage)
+        self.navigationItem.titleView = centerImage
+        
+        self.msgTable.delegate = self
+        self.msgTable.dataSource = self
+        
+        self.msgTable.register(UINib(nibName: "EmptyTableViewCell", bundle: nil), forCellReuseIdentifier: "emptyCell")
+        self.msgTable.register(UINib(nibName: "MessageTableViewCell", bundle: nil), forCellReuseIdentifier: "msgCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.msgData.count == 0 {
+          return 1
+        }else{
+          return self.msgData.count
+        }
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //test
+        if self.msgData.count == 0 {
+          tableView.isScrollEnabled = false
+          tableView.allowsSelection = false
+          let cell = tableView.dequeueReusableCell(withIdentifier: "emptyCell", for: indexPath) as! EmptyTableViewCell
+          return cell
+        }else{
+          let cell = tableView.dequeueReusableCell(withIdentifier: "msgCell", for: indexPath as IndexPath) as! MessageTableViewCell
+          return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+       if self.msgData.count == 0 {
+         return 760
+       }
+        
+        return 44
+    }
+    
+    
+    
+//    func setNavImage(){
+//        self.messageImage = UIImage(named: "message.png")
+//        self.centerImage = UIImageView(image: messageImage)
+//        self.navigationItem.titleView = centerImage
+//    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
