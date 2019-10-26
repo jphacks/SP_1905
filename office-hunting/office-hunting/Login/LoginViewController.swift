6b1de5a38460ab3f8bb73282280e105078f87a37
Fuyan777 <fuyamiwa713@gmail.com>
2019-10-26 10:24:42 +0900

    add layout

---
+++ office-hunting/office-hunting/Login/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  office-hunting
+//
+//  Created by 山田楓也 on 2019/10/26.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
